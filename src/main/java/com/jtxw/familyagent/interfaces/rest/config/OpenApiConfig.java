328a7fc26af61f42b4bcd501fca7dd92cf8ae105
jtxw <jtxw0722@163.com>
2026-06-08 20:22:51 +0800

    style: 修改 OpenAPI bean

--- src/main/java/com/jtxw/familyagent/interfaces/rest/config/OpenApiConfig.java
+++ src/main/java/com/jtxw/familyagent/interfaces/rest/config/OpenApiConfig.java
@@ -1,6 +1,5 @@
 package com.jtxw.familyagent.interfaces.rest.config;
 
-
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
@@ -11,17 +10,21 @@ import org.springframework.context.annotation.Configuration;
  * @Date: 2026/05/17/9:11
  * @Description: OpenAPI 文档配置
  */
-
 @Configuration
 public class OpenApiConfig {
 
-    @Bean
-    public OpenAPI familyRepurchaseOpenAPI() {
+    /**
+     * 创建 OpenAPI 文档配置，用于 Swagger UI 展示 REST Tool API。
+     *
+     * @return OpenAPI 文档描述对象
+     */
+    @Bean(name = "familyRepurchaseOpenAPI")
+    public OpenAPI familyRepurchaseOpenApi() {
         return new OpenAPI()
                 .info(new Info()
                         .title("Family Repurchase Agent API")
                         .version(resolveVersion())
-                        .description("本地优先家庭复购品价格决策 Agent的 REST Tool API。"));
+                        .description("本地优先家庭复购品价格决策 Agent 的 REST Tool API。"));
     }
 
     /**
